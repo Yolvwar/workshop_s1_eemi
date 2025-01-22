7c36dc26cbea421f290ac3234e170b6968d21372
Yoann Mallet <yomallet78@gmail.com>
2025-01-22 03:31:57 +0100

    add(adminAuth): adding AdminAuthenticator to the crud dashboard, redirect if user is not admin role

--- symfony_workshop/src/Entity/User.php
+++ symfony_workshop/src/Entity/User.php
@@ -6,10 +6,13 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -22,12 +25,6 @@ class User
     #[ORM\Column(length: 50)]
     private ?string $firstname = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(length: 100)]
-    private ?string $email = null;
-
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $adress = null;
 
@@ -37,8 +34,21 @@ class User
     #[ORM\Column(nullable: true)]
     private ?int $zip_code = null;
 
-    #[ORM\Column(length: 25, nullable: true)]
-    private ?string $role = null;
+    #[ORM\Column(length: 180)]
+    private ?string $email = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
 
     /**
      * @var Collection<int, Order>
@@ -50,6 +60,7 @@ class User
     {
         $this->orders = new ArrayCollection();
     }
+
 
     public function getId(): ?int
     {
@@ -80,17 +91,6 @@ class User
         return $this;
     }
 
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
 
     public function getEmail(): ?string
     {
@@ -116,6 +116,55 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return list<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
     public function getTown(): ?string
     {
         return $this->town;
@@ -136,18 +185,6 @@ class User
     public function setZipCode(?int $zip_code): static
     {
         $this->zip_code = $zip_code;
-
-        return $this;
-    }
-
-    public function getRole(): ?string
-    {
-        return $this->role;
-    }
-
-    public function setRole(?string $role): static
-    {
-        $this->role = $role;
 
         return $this;
     }
@@ -185,5 +222,14 @@ class User
     public function __toString(): string
     {
         return $this->name . ' ' . $this->firstname; //
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 }
