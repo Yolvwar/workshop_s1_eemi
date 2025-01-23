348e5f00813ecc4684a9acf7ec87f80677834ade
Thomas Sansberro <thomas.sansberro@eemi.com>
2025-01-23 16:11:26 +0100

    ajout page rules

--- symfony_workshop/src/Controller/RulesController.php
+++ symfony_workshop/src/Controller/RulesController.php
@@ -5,13 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Rules;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class RulesController extends AbstractController
 {
     #[Route('/rules', name: 'rules')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $rules = $entityManager->getRepository(Rules::class)->findAll();
         return $this->render('rules/index.html.twig', [
+            'rules' => $rules,
             'controller_name' => 'RulesController',
         ]);
     }
