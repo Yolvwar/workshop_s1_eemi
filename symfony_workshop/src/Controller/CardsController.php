a8aaf2266a04c60104aca96b2e8d8d732b5a86ee
Yolvwar <117321956+Yolvwar@users.noreply.github.com>
2025-01-23 22:58:19 +0100

    Revert "update cards"

--- symfony_workshop/src/Controller/CardsController.php
+++ symfony_workshop/src/Controller/CardsController.php
@@ -2,38 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Card;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Repository\CardRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
 final class CardsController extends AbstractController
 {
     #[Route('/cards', name: 'cards')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(): Response
     {
-        $cards = $entityManager->getRepository(Card::class)->findAll();
-
         return $this->render('cards/index.html.twig', [
-            'cards' => $cards,
             'controller_name' => 'CardsController',
-        ]);
-    }
-
-    #[Route('/cards/{id}', name: 'cards_show')]
-    public function show(int $id, CardRepository $cardRepository): Response
-    {   
-        $cards = $cardRepository->find($id);
-
-
-        if (!$cards) {
-            throw $this->createNotFoundException('Carte non trouvée');
-        }
-
-        return $this->render('cards/show.html.twig', [
-            'cards' => $cards,
         ]);
     }
 }
